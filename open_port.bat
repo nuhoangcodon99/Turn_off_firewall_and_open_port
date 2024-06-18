2d5a474c9b9bd56e74628f6c387f0f9dca45b97c
Nuhoang <tranvienvien51@gmail.com>
2024-06-18 21:56:58 +0700

    Update open_port.bat

--- open_port.bat
+++ open_port.bat
@@ -1,1 +1,20 @@
-netsh advfirewall firewall add rule name="Open Ports" dir=in action=allow protocol=TCP localport=80,443,3306,14445
+@echo off
+
+REM Mở cổng 80 (HTTP)
+netsh advfirewall firewall add rule name="Open Port 80" protocol=TCP dir=in localport=80 action=allow
+netsh advfirewall firewall add rule name="Open Port 80" protocol=TCP dir=out localport=80 action=allow
+
+REM Mở cổng 443 (HTTPS)
+netsh advfirewall firewall add rule name="Open Port 443" protocol=TCP dir=in localport=443 action=allow
+netsh advfirewall firewall add rule name="Open Port 443" protocol=TCP dir=out localport=443 action=allow
+
+REM Mở cổng 3306 (SQL)
+netsh advfirewall firewall add rule name="Open Port 3306" protocol=TCP dir=in localport=3306 action=allow
+netsh advfirewall firewall add rule name="Open Port 3306" protocol=TCP dir=out localport=3306 action=allow
+
+REM Mở cổng 14445 (Custom)
+netsh advfirewall firewall add rule name="Open Port 14445" protocol=TCP dir=in localport=14445 action=allow
+netsh advfirewall firewall add rule name="Open Port 14445" protocol=TCP dir=out localport=14445 action=allow
+
+echo Các cổng đã được mở.
+pause
